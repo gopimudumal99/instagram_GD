f449e2ff1f3b8297c9911c05d2657a2e418622d5
gopimudumal99 <gopimudumal99@gmail.com>
2022-07-24 12:09:10 +0530

    import demmy data from faker

---
+++ utils/FakerData.js
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker/locale/de';
+
+export const USERS = [];
+
+export function createRandomUser() {
+  return {
+    userId: faker.datatype.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
